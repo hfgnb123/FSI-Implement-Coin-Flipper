f5c9d8efa4fc20ef306ad46bc351ec65ac59fdbb
hfgnb123 <hfgnb123@gmail.com>
2023-01-13 01:06:12 -0800

    savepoint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,51 @@
-// TODO: Declare any global variables we need
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
-})
+let numberHeads = 0
+let numberTails = 0
+let pctHeads = 0
+let pctTails = 0
+let total = 0
+const flipButton = document.querySelector("#flip")
+const clearButton = document.querySelector("#clear")
+const statusMessage = document.querySelector(".status")
+const image = document.querySelector("img")
+const numberHeadsCell = document.querySelector("#heads")
+const numberTailsCell = document.querySelector("#tails")
+const pctHeadsCell = document.querySelector("#heads-percent")
+const pctTailsCell = document.querySelector("#tails-percent")
+    flipButton.addEventListener("click", function(e){
+      console.log("I've been clicked")
+      total ++
+      let result = Math.round(Math.random() * 101)
+      console.log("result:", result)
+      if (result < 51){
+        numberHeads ++
+        statusMessage.textContent = "You flipped heads"
+        image.src = "../FSI-Implement-Coin-Flipper/assets/images/penny-heads.jpg"
+      } 
+        else {
+        numberTails ++
+        statusMessage.textContent = "You flipped tails"
+        image.src = "../FSI-Implement-Coin-Flipper/assets/images/penny-tails.jpg"
+      }
+      console.log(numberHeads, numberTails)
+      pctHeads = Math.round(numberHeads/total * 100)
+      pctTails = Math.round(numberTails/total * 100)
+      console.log("percentages", pctHeads, pctTails)
+      numberHeadsCell.textContent = `${numberHeads}`
+      numberTailsCell.textContent = `${numberTails}`
+      pctHeadsCell.textContent = `${pctHeads}%`
+      pctTailsCell.textContent = `${pctTails}%`
+    })
+    clearButton.addEventListener("click", function(e){
+      console.log("i was clicked")
+      total = 0
+      pctHeads = 0
+      pctTails = 0
+      numberHeads = 0
+      numberTails = 0
+      numberHeadsCell.textContent = `${numberHeads}`
+      numberTailsCell.textContent = `${numberTails}`
+      pctHeadsCell.textContent = `${pctHeads}%`
+      pctTailsCell.textContent = `${pctTails}%`
+      statusMessage.textContent = "Let's Get Rolling"
+      image.src = "./assets/images/penny-heads.jpg"
+    })
